5f9473aaf45db6bcb9b2ba066b80b6a94e2859ed
Bernie Chiu <bernie_chiu@hotmail.com>
2014-01-20 23:09:13 +0800

    Modify for Heroku migration

--- db/migrate/20140119162522_change_datatype_on_amount_to_equipment.rb
+++ db/migrate/20140119162522_change_datatype_on_amount_to_equipment.rb
@@ -1,6 +1,12 @@
 class ChangeDatatypeOnAmountToEquipment < ActiveRecord::Migration
   def up
     change_column :equipment, :amount, :integer
+
+    connection.execute(%q{
+      ALTER TABLE equipment
+      ALTER COLUMN amount
+      type integer using cast(amount as integer)
+    })
   end
 
   def down
